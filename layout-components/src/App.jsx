697fec43626ec83932d17a9c082e6f1b07f03d04
serhatbek <serhat.bek@gmail.com>
2024-06-04 14:32:19 +0300

    Screen Splitter

--- layout-components/src/App.jsx
+++ layout-components/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SplitScreen } from './components';
+
+const LeftSideComp = () => {
+  return <h2 style={{ backgroundColor: 'burlywood' }}>I'm left side</h2>;
+};
+
+const RightSideComp = () => {
+  return <h2 style={{ backgroundColor: 'crimson' }}>I'm right side.</h2>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SplitScreen Left={LeftSideComp} Right={RightSideComp} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ layout-components/src/components/SplitScreen.jsx
@@ -0,0 +1,24 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Panel = styled.div`
+  flex: 1;
+`;
+
+const SplitScreen = ({ Left, Right }) => {
+  return (
+    <Container>
+      <Panel>
+        <Left />
+      </Panel>
+
+      <Panel>
+        <Right />
+      </Panel>
+    </Container>
+  );
+};
+export default SplitScreen;
