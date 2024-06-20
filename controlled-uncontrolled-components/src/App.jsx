eeb8f6c9499e5f79b9513133334417e58d94772c
serhatbek <serhat.bekk@gmail.com>
2024-06-21 00:47:13 +0300

    Controlled Modals

--- controlled-uncontrolled-components/src/App.jsx
+++ controlled-uncontrolled-components/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import ControlledForm from './components/ControlledForm';
+import UncontrolledModal from './components/UncontrolledModal';
 import UncontrolledForm from './components/UncontrolledForm';
+import ControlledModal from './components/ControlledModal';
 
 function App() {
+  const [shouldDisplay, setShouldDisplay] = useState(false);
   return (
     <>
-      <UncontrolledForm />
+      {/* <UncontrolledForm /> */}
       <br />
-      <ControlledForm />
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledModal /> */}
+      <ControlledModal
+        shouldDisplay={shouldDisplay}
+        onClose={() => setShouldDisplay(false)}
+      ></ControlledModal>
+      <button onClick={() => setShouldDisplay(!shouldDisplay)}>
+        {shouldDisplay ? 'Hide Modal' : 'Display Modal'}
+      </button>
     </>
   );
 }
