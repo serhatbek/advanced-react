067bab12a3a51df7269b8190e109aa5272721c90
serhatbek <serhat.bekk@gmail.com>
2024-06-21 17:43:56 +0300

    Uncontrolled Flows

--- controlled-uncontrolled-components/src/App.jsx
+++ controlled-uncontrolled-components/src/App.jsx
@@ -3,22 +3,55 @@ import ControlledForm from './components/ControlledForm';
 import UncontrolledModal from './components/UncontrolledModal';
 import UncontrolledForm from './components/UncontrolledForm';
 import ControlledModal from './components/ControlledModal';
+import UncontrolledFlow from './components/UncontrolledFlow';
+
+const StepOne = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepTwo = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #2</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepThree = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
 
 function App() {
-  const [shouldDisplay, setShouldDisplay] = useState(false);
+  // const [shouldDisplay, setShouldDisplay] = useState(false);
   return (
     <>
       {/* <UncontrolledForm /> */}
-      <br />
+      {/* <br /> */}
       {/* <ControlledForm /> */}
       {/* <UncontrolledModal /> */}
-      <ControlledModal
+
+      {/* <ControlledModal
         shouldDisplay={shouldDisplay}
         onClose={() => setShouldDisplay(false)}
       ></ControlledModal>
       <button onClick={() => setShouldDisplay(!shouldDisplay)}>
         {shouldDisplay ? 'Hide Modal' : 'Display Modal'}
-      </button>
+      </button> */}
+
+      <UncontrolledFlow>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledFlow>
     </>
   );
 }
